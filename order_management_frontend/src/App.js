52335b34b5aee2f4eee98fc0f64e8b11dcd05cd1
rahul <rahuls@shriffle.com>
2024-12-16 16:49:08 +0530

    Update frontend routes and add token validation before redirection

--- order_management_frontend/src/App.js
+++ order_management_frontend/src/App.js
@@ -1,46 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Auth/Login';
-import Signup from './components/Auth/Signup';
-import ReferralForm from './components/Referral/ReferralForm';
-import RestaurantsPage from './components/Restaurant/RestaurantsPage';
-import MenuPage from './components/Menu/MenuPage';
-import MenuShow from './components/Menu/MenuShow';
-import MenuForm from './components/Menu/MenuForm';
-import { CartProvider } from './components/context/CartContext';
-import CartPage from './components/context/CartPage';
-import CheckoutPage from './components/context/CheckoutPage';
-import OrdersPage from './components/Order/OrdersPage';
-import OrderDetailsPage from './components/Order/OrderDetailsPage';
-import RestaurantLogin from './components/Auth/RestaurantLogin';
-import RestaurantSignup from './components/Auth/RestaurantSignup';
-import OrderTrackingPage from './components/Order/OrderTrackingPage';
-
-import UserDashboard from './components/User/UserDashboard';
-import DriverDashboard from './components/Driver/DriverDashboard';
-import CreateRestaurant from './components/Restaurant/CreateRestaurant';
-import Navbar from './components/Navbar/Navbar';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import { CartProvider } from "./components/context/CartContext";
+import AppRoutes from "./AppRoutes"; // Import the routes
 
 const App = () => (
-  <CartProvider> {/* Wrap the app with CartProvider */}
+  <CartProvider>
     <Router>
-      <Navbar /> {/* Include Navbar here */}
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/users" element={<UserDashboard />} />
-        <Route path="/restaurants" element={<RestaurantsPage />} />
-        <Route path="/restaurants/new" element={<CreateRestaurant />} />
-        <Route path="/restaurants/:restaurantId/menus" element={<MenuPage />} />
-        <Route path="/restaurants/:restaurantId/menus/new" element={<MenuForm />} />
-        <Route path="/restaurants/:restaurantId/menus/:id/edit" element={<MenuForm />} />
-        <Route path="/restaurants/:restaurantId/menus/:id/" element={<MenuShow />} />
-        <Route path="/restaurants/:restaurantId/orders" element={<OrdersPage />} />
-        <Route path="/restaurants/:restaurantId/orders/:orderId" element={<OrderDetailsPage />} />
-        // <Route path="/restaurants/:restaurantId/orders/" element={<OrderDetailsPage />} />
-        <Route path="/cart" element={<CartPage />} /> {/* Route to Cart Page */}
-
-
-      </Routes>
+      <Navbar />
+      <AppRoutes /> {/* Use the separated routes */}
     </Router>
   </CartProvider>
 );
---
+++ order_management_frontend/src/AppRoutes.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/Auth/ProtectedRoute";
+
+import Login from "./components/Auth/Login";
+import Signup from "./components/Auth/Signup";
+import UserDashboard from "./components/User/UserDashboard";
+import DriverDashboard from "./components/Driver/DriverDashboard";
+import ReferralForm from "./components/Referral/ReferralForm";
+import RestaurantsPage from "./components/Restaurant/RestaurantsPage";
+import CreateRestaurant from "./components/Restaurant/CreateRestaurant";
+import MenuPage from "./components/Menu/MenuPage";
+import MenuForm from "./components/Menu/MenuForm";
+import MenuShow from "./components/Menu/MenuShow";
+import CartPage from "./components/context/CartPage";
+import CheckoutPage from "./components/context/CheckoutPage";
+import OrdersPage from "./components/Order/OrdersPage";
+import OrderDetailsPage from "./components/Order/OrderDetailsPage";
+import RestaurantLogin from "./components/Auth/RestaurantLogin";
+import RestaurantSignup from "./components/Auth/RestaurantSignup";
+import OrderTrackingPage from "./components/Order/OrderTrackingPage";
+
+const AppRoutes = () => (
+  <Routes>
+    {/* Public Routes */}
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/restaurants" element={<RestaurantsPage />} />
+    <Route path="/restaurant/login" element={<RestaurantLogin />} />
+    <Route path="/restaurant/signup" element={<RestaurantSignup />} />
+
+    {/* Protected Routes */}
+    <Route path="/users" element={ <ProtectedRoute> <UserDashboard /> </ProtectedRoute> }/>
+    <Route path="/drivers" element={ <ProtectedRoute> <DriverDashboard /> </ProtectedRoute> }/>
+    <Route path="/referral" element={ <ProtectedRoute> <ReferralForm /> </ProtectedRoute> }/>
+    <Route path="/restaurants/new" element={ <ProtectedRoute> <CreateRestaurant /> </ProtectedRoute> }/>
+    <Route path="/restaurants/:restaurantId/menus" element={ <ProtectedRoute> <MenuPage /> </ProtectedRoute> }/>
+    <Route path="/restaurants/:restaurantId/menus/new" element={ <ProtectedRoute> <MenuForm /> </ProtectedRoute> }/>
+    <Route path="/restaurants/:restaurantId/menus/:id/edit" element={ <ProtectedRoute> <MenuForm /> </ProtectedRoute> }/>
+    <Route path="/restaurants/:restaurantId/menus/:id" element={ <ProtectedRoute> <MenuShow /> </ProtectedRoute> }/>
+    <Route path="/restaurants/:restaurantId/orders" element={ <ProtectedRoute> <OrdersPage /> </ProtectedRoute> }/>
+    <Route path="/restaurants/:restaurantId/orders/:orderId" element={ <ProtectedRoute> <OrderDetailsPage /> </ProtectedRoute> }/>
+    <Route path="/cart" element={ <ProtectedRoute> <CartPage /> </ProtectedRoute> }/>
+    <Route path="/checkout" element={ <ProtectedRoute> <CheckoutPage /> </ProtectedRoute> }/>
+    <Route path="/order/tracking" element={ <ProtectedRoute> <OrderTrackingPage /> </ProtectedRoute> }/>
+  </Routes>
+);
+
+export default AppRoutes;
